08c4ab198f658e52b77fd53e7c28ea284830c81c
Pranay <pranay.rungta@gmail.com>
2024-10-20 18:38:50 +0530

    added story and products; home cmplete

---
+++ src/components/Product-carousel.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./../styles/responsive.css"
+import "./../styles/style.css"
+
+export default function ProductCarousel() {
+  const products = [
+    {link:"/products/paper-straw/",      text:"Paper Straw"       , imgsrc: "/products/paper.jpg"           },
+    {link:"/products/bobbin/",           text:"Bobbin"            , imgsrc: "/products/Bobbin.png"          },
+    {link:"/products/paper-cup-blanks/", text:"Paper Cup Blanks"  , imgsrc: "/products/Blanks.jpg"          },
+    {link:"/products/paper-tissu/",      text:"Paper Tissue"      , imgsrc: "/products/Tissue.png"          },
+    {link:"/products/laminated-lid/",    text:"Laminated LID"     , imgsrc: "/products/Lids.png"            },
+    {link:"/products/coated-paper/",     text:"Coated Paper"      , imgsrc: "/products/Coated-Paper-2.png"  },
+    {link:"/products/paper-cups/",       text:"Paper Cups"        , imgsrc: "/products/1.png"               },
+    {link:"/products/test/",             text:"Paper Plate Blanks", imgsrc: "/products/9.png"               },
+  ];
+  return (
+    <div className="featured_products">
+    <div className="container">
+      <h2><span>Our</span> Products</h2>
+      <div className="pro-row">
+      <div className="productslide row">  {/*className='row' added later*/}
+        {products.map((p,i)=>{return (
+          <div key={i} className='col-4'>  {/*className='col-4' added later*/}
+          <div className="pro-con">
+            <div className="prod-img">
+              <a href={p.link}>
+                <img src={p.imgsrc} alt={p.text}/>
+              </a>
+            </div>
+            <div className="pro-sec">
+              <h3><a href={p.link}>{p.text}</a></h3>
+            </div>
+          </div>
+          </div>
+        )})}
+      </div>
+      </div>
+    </div>
+    </div>)
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,45 @@
 import * as React from "react"
 import Layout from "../components/Layout"
-import HomeBanner from "../components/HomeBanner"
+import Story from "../components/Story"
+import ProductCarousel from "../components/Product-carousel"
+import "./../styles/homebanner.css"
+
+function HomeBanner() {
+  let files = ['Paper-Cup-1.png', 'Paper-Plate.png', 'Coated-Paper-1.png',
+  'Aluminium-Foil-Container.png', 'Paper-Straws.png', 'Machinery.png' ]
+  return (<div>
+    <div id="carouselExample" className="carousel slide home_banner">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+        <img src={"/home/" + files[0]}
+            className="d-block w-100" alt="..."/>
+        </div>
+        {files.slice(1).map((file, index)=>{ return (
+          <div className="carousel-item" key={index}>
+          <img src={"/home/" + file}
+              className="d-block w-100" alt="..."/>
+          </div>
+        )})}
+      </div>
+      <button className="carousel-control-prev" type="button"
+              data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button"
+              data-bs-target="#carouselExample" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+    </div>)
+}
 
 export default function Home() {
   return (<Layout>
     <HomeBanner/>
-    <div>
-      Hello world!
-    </div>
+    <Story/>
+    <ProductCarousel/>
+    <div></div>
   </Layout>)
 }
