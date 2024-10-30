0e9b006e931897e44486492512bcb5d731d79f72
Pranay <pranay.rungta@gmail.com>
2024-10-30 17:10:40 +0530

    machines and products content completed

--- src/templates/machines.js
+++ src/templates/machines.js
@@ -4,16 +4,261 @@ import { table, get_data, breadcrum, featured } from "./prod-data";
 
 function machines_data(){
   return {
-    "coating-machine"              : {imgsrc : "/machines/m-5.jpg"  , name : "Coating machine"             , data : [] },
-    "slitting-machine"             : {imgsrc : "/machines/m-4.jpg"  , name : "Slitting machine"            , data : [] },
-    "flexo-paper-printing-machine" : {imgsrc : "/machines/m-3.jpg"  , name : "Flexo Paper Printing Machine", data : [] },
-    "die-cutting-machine"          : {imgsrc : "/machines/m-2.png"  , name : "Die cutting Machine"         , data : [] },
-    "paper-cup-machine"            : {imgsrc : "/machines/m-1.png"  , name : "Paper Cup Machine"           , data : [] },
+    "coating-machine"              : {imgsrc : "/machines/m-5.jpg"  ,
+                                      name : "Coating machine"             },
+    "slitting-machine"             : {imgsrc : "/machines/m-4.jpg"  ,
+                                      name : "Slitting machine"            },
+    "flexo-paper-printing-machine" : {imgsrc : "/machines/m-3.jpg"  ,
+                                      name : "Flexo Paper Printing Machine"},
+    "die-cutting-machine"          : {imgsrc : "/machines/m-2.png"  ,
+                                      name : "Die cutting Machine"         },
+    "paper-cup-machine"            : {imgsrc : "/machines/m-1.png"  ,
+                                      name : "Paper Cup Machine"           },
   }
 }
 
-function machpage(slug){
-  const alldata = get_data(machines_data(), slug)
+
+function coating_machine(){
+  const df = [['Capacity', '120 metre per min(Production)'],
+    ['Automation Grade',            'Semi-Automatic'],
+    ['Applicable Coating Material', 'PE'],
+    ['Max Width of Coating',        '1200 mm'],
+    ['Max Working Speed',           '150 m/min']]
+  return (<div class="col-sm-6">
+    <h2>Coating machine</h2>
+    {table(df)}
+    <hr/>
+  </div>)
+}
+
+function slitting(){
+  return (<div class="col-sm-6">
+  <h2>Slitting machine</h2>
+  <p>
+    This machine has the cutting width of 30-1250mm, which includes
+    the diameter of less than 1300 mm. Furthermore, the specification
+    of the width finished is 30/1250mm. The offered machine has entire
+    productive things to handle high production along with supporting
+    customized deliveries. Besides, the machine paper rolls tight end
+    face flushes.
+  </p>
+  <p>Features:</p>
+  <ul>
+    <li>The rewinding parts adopted double servo motors</li>
+    <li>Structure, slide, and one line production</li>
+    <li>Require dia. is 100mm with dynamic balance</li>
+  </ul>
+  <p>Technical Parameter:</p>
+  <ul>
+    <li>The range of gram weight: 80-400 gsm.</li>
+    <li>The cutting width: 30-1250mm</li>
+    <li>The diameter cutting is less than 1300 mm</li>
+    <li>
+      Finished specification width: 30/1250mm , the diameter of paper
+      is less than 800mm, After the cut end deviation 0.25mm, Paper
+      roll tightness of consistent, end face flush
+    </li>
+    <li>The deviation of cutting width: 0.25mm</li>
+    <li>The speed of operation: 75-150mtr/min</li>
+    <li>The suitable material: 50-400 gsm</li>
+    <li>
+      The range of rewinding: from 30mm-1250mm within could adjust ,
+      the circular knife is 14 pieces
+    </li>
+    <li>
+      The mode of taking-up equipment: it adopts double rewinding
+      shaft, it could choose different roll according to your
+      requirement, it is equipped with air-expansion ( 2 pieces of 3
+      inch air-expansion rewinding shaft )
+    </li>
+    <li>
+      In the system : Correct an error to make sure good condition ,
+      also the scrape edge is 3-8mm
+    </li>
+    <li>
+      The unwinding parts: it could adjust from left to right , the
+      air-expansion shafts is hard to make sure handing is safe and
+      convenient
+    </li>
+    <li>Feeding tension control : 20kg magnetic powder control</li>
+    <li>
+      Automatic count meter, alarm and halt equipment , the whole is
+      equipped with safe signal and shield to make sure operate safety
+    </li>
+    <li>
+      The rewinding parts : it is adopted double servo motors control
+      , the dia. of rewinding is less than 800mm
+    </li>
+    <li>
+      The whole adopts superior steel plate (thickness is 30mm) and
+      channel steel structure, slide adopts linear bearing
+    </li>
+    <li>It adopts the dia. is 100mm dynamic balance</li>
+    <li>
+      The mainframe adopts 5.5km stepless frequency speed regulation
+      is 0-150m/min( it could adjust according to different material )
+    </li>
+  </ul>
+  <p>Additional Information:Item Code: PSM-1300</p>
+  <hr />
+  </div>)
+}
+
+function flexo(){
+  const df = [['Model Number', 'RPC-930-A'],
+    ['Max Printing Length',    '500-1000 (mm)'],
+    ['Material',               'Iron'],
+    ['Colour Printing',        'Colour Printing'],
+    ['Weight',                 '7 Ton'],
+    ['Printing Speed',         '110-120/Meter/Min']]
+  return (<div class="col-sm-6">
+  <h2>Flexo Paper Printing Machine</h2>
+  {table(df)}
+  <p>
+    Since our commencement, our firm is engaged to introduce a premium
+    quality array of 4 Colour Flexo Printing Machine. The offered
+    machine has a Mitsubishi automatic tension controller for
+    unwinding the prating status. This machine is equipped with four
+    colour printing station with automatic tension controller for
+    rewinding. Also, the 4 set of printing cylinder and IR dryer for
+    every printing station makes the machine highly productive. It
+    required less space for installation.
+  </p>
+  <p><strong>Features:</strong></p>
+  <ul>
+    <li>Low maintenance and power efficient</li>
+    <li>±0.15mm chromatography precision:</li>
+    <li>1500mm is the maximum diameter rewinding</li>
+  </ul>
+  <p><strong>Machine Units List:</strong></p>
+  <ul>
+    <li>4 colours printing station</li>
+    <li>Laminating</li>
+    <li>Mitsubishi automatic tension controller for unwinding</li>
+    <li>Automatic tension controller for rewinding made in China</li>
+    <li>4 pcs of ceramic Anilox (line number on your request)</li>
+    <li>4 set of printing cylinder</li>
+    <li>IR dryer for every printing station</li>
+    <li>With web guide &amp; corona treatment</li>
+  </ul>
+  <p><strong>Machine Specification:</strong></p>
+  <ul>
+    <li>Max. Printing speed: 60m/min</li>
+    <li>Printing colour: 4 colours</li>
+    <li>Max paper roll width: 930mm</li>
+    <li>Max printing width: 920mm</li>
+    <li>Max unwinding diameter: 1500mm</li>
+    <li>Max rewinding diameter: 1500mm</li>
+    <li>Printing girth: 180-420mm</li>
+    <li>Chromatography precision: ±0.15mm</li>
+  </ul>
+  <p><strong>Additional Information:</strong> <br/>
+    Item Code: RPC-930-A</p>
+  <hr />
+  </div>)
+}
+
+function die_cutting(){
+  return (<div class="col-sm-6">
+    <h2>Die cutting Machine</h2>
+    <p>
+      The offered machine delivers working speed of 100-130 punches per
+      minute. It has the ability to die-cutting, creasing, and embossing
+      for various shapes. In addition, the machine has motor to assure
+      steady and accurate conveying of the cut. Further, the smart photo
+      electricity sensor checks the colour code of printing to ensure
+      fix the paper position accurately.
+    </p>
+    <p>
+      The working speed is 100-130 Punches per minute the level of the
+      capacity in China is 120 times per minute. It has the ability to
+      die-cutting, creasing and embossing for various shapes
+    </p>
+    <p>
+      Paper conveyed by servo motor to ensures steady and accurate
+      conveying. Photo electricity sensor checks the colour code of
+      printing to ensure fix the paper position accurately
+    </p>
+    <p>
+      Electric control system uses PLC and HMI. Equipped with safety
+      refinements to make acousto - optical alarming and auto stop
+      when some faults.
+    </p>
+    <p>
+      Worm wheel and worm transmission system ensures the powerful and
+      steady pressure;it is very convenient and accurate to adjust
+      pressure through HMI
+    </p>
+    <p>
+      Lubricating with oil circulation and alarming when lack of oil to
+      ensure smooth running and prolong the machine life. The
+      die-cutting frame uses pneumatic locking system to convenient
+      operation. Die-cutting down steel board from Taiwan is accurate
+      and lasts forever
+    </p>
+    <p>
+      It adapts electric control pneumatic clutch to protect the machine
+      when start and emergency stop. The flywheel provides a mounting
+      location to the clutch and also can increase the pressure
+    </p>
+    <p><strong>Features:</strong></p>
+    <p>Transmission system control power and pressure system</p>
+    <p>Safety refinements added such as alarming and indicating</p>
+    <p>Die-cutting frame uses pneumatic locking system</p>
+    <p>
+      <strong>Specifications:</strong>
+      TypeRDC-950ACut Precision:± 0.10mm <br/>
+      Production capacity:100-130 PUNCHES/min  <br/>
+      Maximum cutting size:950 x550mm <br/>
+      Max Paper Convey Width:970mm <br/>
+      Suitable Material:100-600g/m2Maximum <br/>
+      Pressure:300T <br/>
+      Total Power:15HP, 3PH(380V) <br/>
+      Total Weight:8.0 MT <br/>
+      Overall Dimension:4100 x 2000 x 1850 mm <br/>
+      Working Air Source:Air pressure 0.5-0.7Mpa (Need to buy Air compressor)
+    </p>
+    <hr/>
+  </div>)
+}
+
+function paper_cup(){
+  const df = [['Frequency', '50-60 Hz'     ],
+    ['Machines Speed',      '65-75 PPM'    ],
+    ['Machine Weight',      '2000-3000 Kg' ],
+    ['Production Capacity', '60-75 cup/min'],
+    ['Power Consumption',   '3.50 kW'      ],
+    ['Automation Grade',    'Automatic'    ],
+    ['Warranty',            '1 Year'       ],
+    ['Weight 2100Kgs',      '2500 KG'      ],
+    ['Voltage',             '210- 280 V'   ]]
+  return (<div class="col-sm-6">
+  <h2>Paper Cup Machine</h2>
+  <p>
+    We offer best quality Fully Automatic High- Speed Paper Cup Making
+    Machine at an affordable rate with overall technical support.
+  </p>
+  {table(df)}
+  <hr />
+  </div>)
+}
+
+
+function machine_details(slug){
+  const maps = {
+    'coating-machine' : coating_machine,
+    'die-cutting-machine' : die_cutting,
+    'paper-cup-machine' : paper_cup,
+    'flexo-paper-printing-machine' : flexo,
+    'slitting-machine': slitting
+  }
+  if (slug in maps)
+    return maps[slug]()
+  return (<div></div>)
+}
+
+export default function Machines({pageContext}) {
+  const alldata = get_data(machines_data(), pageContext.slug)
   const curprod = alldata.prod;
 
   return (<Layout>
@@ -34,16 +279,11 @@ function machpage(slug){
           className="img-responsive img-thumbnail" />
         </div>
       </div>
-      {/* {product_details(slug)} */}
+      {machine_details(pageContext.slug)}
     </div>
     </div>
     </div>
-    {featured(alldata.fp)}
+    {featured(alldata.fp, true)}
+    <div className="newsletter_area"></div>
   </Layout>)
 }
-
-
-export default function Machines({pageContext}) {
-  return machpage(pageContext.slug)
-}
-
--- src/templates/prod-data.js
+++ src/templates/prod-data.js
@@ -15,21 +15,6 @@ export function table(df){
   </div>)
 }
 
-function products_data(){
-  return {
-    "paper-plate-blanks"       : {imgsrc : "/products/9.png"             , name : "Paper Plate Blanks"      },
-    "aluminium-foil-container" : {imgsrc : "/products/2.png"             , name : "Aluminium Foil Container"},
-    "bobbin"                   : {imgsrc : "/products/Bobbin.png"        , name : "Bobbin"                  },
-    "laminated-lid"            : {imgsrc : "/products/Lids.png"          , name : "Laminated LID"           },
-    "paper-cups"               : {imgsrc : "/products/1.png"             , name : "Paper Cups"              },
-    "paper-cup-blanks"         : {imgsrc : "/products/Blanks.jpg"        , name : "Paper Cup Blanks"        },
-    "paper-straw"              : {imgsrc : "/products/paper.jpg"         , name : "Paper Straw"             },
-    "coated-paper"             : {imgsrc : "/products/Coated-Paper-2.png", name : "Coated Paper"            },
-    "aluminium-home-foil"      : {imgsrc : "/products/Home-Foil.png"     , name : "Aluminium Home Foil"     },
-    "paper-tissue"             : {imgsrc : "/products/Tissue.png"        , name : "Paper Tissue"            } }
-}
-
-
 export function get_data(fp, slug){
   // let fp = products_data();
   let prod = fp[slug];
@@ -38,7 +23,6 @@ export function get_data(fp, slug){
     ([slug, value]) => ({slug, ...value }));
   return {prod: prod, fp:fp};
 }
-
 
 export function breadcrum(name) {
   return (<div className="breadcrumb_area">
@@ -71,39 +55,79 @@ export function breadcrum(name) {
   </div>)
 }
 
+export function featured(fp, machines=false){
+  let route = '';
+  if (machines){ route = 'machines'; }
+  else { route = 'products';}
+
+  return (<div className="featured_products related_product">
+  <div className="container">
+    <h2><span>Related</span> Products</h2>
+    <div className="pro-row">
+    <div className="productslide">
+    {fp.map((p,index)=> {return (
+      <div key={index}>
+      <div className="pro-con">
+        <div className="prod-img">
+          <a href={`/${route}/${p.slug}`}>
+          <img src={p.imgsrc} alt="" />
+          </a>
+        </div>
+        <div className="pro-sec">
+          <h3><a href={`/${route}/${p.slug}`}>
+            {p.name}
+          </a></h3>
+        </div>
+      </div>
+      </div>
+    )})}
+    </div>
+    </div>
+  </div>
+  </div>)
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function products_data(){
+  return {
+    "paper-plate-blanks"       : {imgsrc : "/products/9.png"             , name : "Paper Plate Blanks"      },
+    "aluminium-foil-container" : {imgsrc : "/products/2.png"             , name : "Aluminium Foil Container"},
+    "bobbin"                   : {imgsrc : "/products/Bobbin.png"        , name : "Bobbin"                  },
+    "laminated-lid"            : {imgsrc : "/products/Lids.png"          , name : "Laminated LID"           },
+    "paper-cups"               : {imgsrc : "/products/1.png"             , name : "Paper Cups"              },
+    "paper-cup-blanks"         : {imgsrc : "/products/Blanks.jpg"        , name : "Paper Cup Blanks"        },
+    "paper-straw"              : {imgsrc : "/products/paper.jpg"         , name : "Paper Straw"             },
+    "coated-paper"             : {imgsrc : "/products/Coated-Paper-2.png", name : "Coated Paper"            },
+    "aluminium-home-foil"      : {imgsrc : "/products/Home-Foil.png"     , name : "Aluminium Home Foil"     },
+    "paper-tissue"             : {imgsrc : "/products/Tissue.png"        , name : "Paper Tissue"            } }
+}
 
 function tab_only(slug){
   let d = {
-    "paper-plate-blanks"       : [['Size', 'As per Requirement'], ['Bag Packing', '30 Kg']],
-    "aluminium-foil-container" : [['Size', '120 ml', '230 ml', '250 ml', '450 ml', '660 ml', '750 ml'], ['Packing(Pcs)', '100', '100', '100', '100', '100', '100'], ['Box Packing (Pcs)', '3750', '1650', '1750', '1750', '1750', '1750'], ['Thickness(Micron)', '34', '34', '34', '35', '38', '38']],
-    "bobbin"                   : [['Size', 'As per Requirement'], ['Packing (in Kgs)', 'As per Requirement']],
-    "laminated-lid"            : [['Size', '120 ml', '150 ml', '200 ml', '230 ml', '250 ml', '450 ml', '660 ml', '750 ml'], ['Packing (Pcs)', '100', '100', '100', '100', '100', '100', '100', '100'], ['Box Packing (Pcs)', 'As per Container Quantity', 'As per Container Quantity', 'As per Container Quantity', 'As per Container Quantity', 'As per Container Quantity', 'As per Container Quantity', 'As per Container Quantity', 'As per Container Quantity']],
-    "paper-cups"               : [['Size', '40 ml', '50 ml', '60 ml', '80 ml', '100 ml', '150 ml', '210 ml', '250 ml'], ['Packing(Pcs)', '100', '100', '100', '100', '100', '100', '100', '100'], ['Box Packing(Pcs)', '7800', '6600', '6600', '5500', '4500', '2500', '2500', '2500'], ['Thickness(GSM)', '152-162', '152-162', '152-162', '152-162', '152-162', '152-162', '152-162', '152-162']],
-    "paper-straw"              : [['Size', '6 mm', '8 mm', '10 mm'], ['Packing (Pcs)', '50', '50', '50'], ['Box Packing (Pcs)', '5000', '5000', '5000']],
-    "aluminium-home-foil"      : [['Size', '9 Mtrs', '72 Mtrs', '72 No.', '75 No.'], ['Micron', '10.5 Micron', '10.5 Micron', '10.5 Micron', '10.5 Micron'], ['Box Packing (Pcs)', '100', '25', '25', '25']],
-    "paper-tissue"             : [['Size', '20 X 20', '24 X 24', '30 X 30', '33 X 33'], ['Packing (Pcs)', '50', '50', '50', '50'], ['Box Packing (Pcs)', '500', '500', '500', '500']]
+    "paper-plate-blanks"       : [['Size', 'As per Requirement'],
+              ['Bag Packing', '30 Kg']],
+    "aluminium-foil-container" : [['Size', '120 ml', '230 ml', '250 ml', '450 ml', '660 ml', '750 ml'],
+              ['Packing(Pcs)', '100', '100', '100', '100', '100', '100'],
+              ['Box Packing (Pcs)', '3750', '1650', '1750', '1750', '1750', '1750'],
+              ['Thickness(Micron)', '34', '34', '34', '35', '38', '38']],
+    "bobbin"                   : [['Size', 'As per Requirement'],
+              ['Packing (in Kgs)', 'As per Requirement']],
+    "laminated-lid"            : [['Size', '120 ml', '150 ml', '200 ml', '230 ml', '250 ml', '450 ml', '660 ml', '750 ml'],
+              ['Packing (Pcs)', '100', '100', '100', '100', '100', '100', '100', '100'],
+              ['Box Packing (Pcs)', 'As per Container Quantity', 'As per Container Quantity', 'As per Container Quantity', 'As per Container Quantity', 'As per Container Quantity', 'As per Container Quantity', 'As per Container Quantity', 'As per Container Quantity']],
+    "paper-cups"               : [['Size', '40 ml', '50 ml', '60 ml', '80 ml', '100 ml', '150 ml', '210 ml', '250 ml'],
+              ['Packing(Pcs)', '100', '100', '100', '100', '100', '100', '100', '100'],
+              ['Box Packing(Pcs)', '7800', '6600', '6600', '5500', '4500', '2500', '2500', '2500'],
+              ['Thickness(GSM)', '152-162', '152-162', '152-162', '152-162', '152-162', '152-162', '152-162', '152-162']],
+    "paper-straw"              : [['Size', '6 mm', '8 mm', '10 mm'],
+              ['Packing (Pcs)', '50', '50', '50'],
+              ['Box Packing (Pcs)', '5000', '5000', '5000']],
+    "aluminium-home-foil"      : [['Size', '9 Mtrs', '72 Mtrs', '72 No.', '75 No.'],
+              ['Micron', '10.5 Micron', '10.5 Micron', '10.5 Micron', '10.5 Micron'],
+              ['Box Packing (Pcs)', '100', '25', '25', '25']],
+    "paper-tissue"             : [['Size', '20 X 20', '24 X 24', '30 X 30', '33 X 33'],
+              ['Packing (Pcs)', '50', '50', '50', '50'],
+              ['Box Packing (Pcs)', '500', '500', '500', '500']]
   }
   const pd = products_data();
   return (<div class="col-sm-8">
@@ -134,6 +158,7 @@ function coated_paper(name){
   </div>)
 }
 
+
 function product_details(slug){
   let maps = {"paper-cup-blanks" : paper_cup_banks,
               "coated-paper"     : coated_paper};
@@ -142,38 +167,6 @@ function product_details(slug){
     return maps[slug](name);
   }
   return tab_only(slug)
-}
-
-
-
-
-
-export function featured(fp){
-  return (<div className="featured_products related_product">
-  <div className="container">
-    <h2><span>Related</span> Products</h2>
-    <div className="pro-row">
-    <div className="productslide">
-    {fp.map((p,index)=> {return (
-      <div key={index}>
-      <div className="pro-con">
-        <div className="prod-img">
-          <a href={"/products/"+p.slug}>
-          <img src={p.imgsrc} alt="" />
-          </a>
-        </div>
-        <div className="pro-sec">
-          <h3><a href={"/products/"+p.slug}>
-            {p.name}
-          </a></h3>
-        </div>
-      </div>
-      </div>
-    )})}
-    </div>
-    </div>
-  </div>
-  </div>)
 }
 
 
@@ -204,5 +197,6 @@ export function prodpage(slug){
     </div>
     </div>
     {featured(alldata.fp)}
+    <div className="newsletter_area"></div>
   </Layout>)
 }
